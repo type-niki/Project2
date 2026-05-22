7a575ad46aae22f64b43d7415f4bafcd32fac630
type-niki <rennenekesa7@gmail.com>
2026-05-22 18:43:58 +0300

    Python basics

---
+++ var.py
@@ -0,0 +1,18 @@
+name = 'Renne'
+print(name, type(name)) 
+#prints the value of name and its data type
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 21
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score)) 
+
+
+
+my_str = 'hello world'
+print(my_str.replace('hello', 'hi'))  
